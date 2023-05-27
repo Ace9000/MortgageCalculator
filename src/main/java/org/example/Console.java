efbe9500ba2a0456c1f3b36f388f76537b9a6a99
Anastas Acevski <anastas.acevski@outlook.com>
2023-05-27 19:41:25 +0200

    MortgageCalculator

---
+++ src/main/java/org/example/Console.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Console {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static double readNumber(String prompt) {
+        return scanner.nextDouble();
+    }
+    public static double readNumber(String prompt, double min, double max) {
+        double value;
+        while (true) {
+            System.out.println(prompt);
+            value = scanner.nextDouble();
+            if( value >= min && value <= max )
+                break;
+            System.out.println("Enter a value between " + min + " and " +max);
+        }
+        return value;
+    }
+}
